2e1ec23b22415d210d6a83982d648614a139282c
Danial <danial.mail.gh@gmail.com>
2023-07-09 22:31:45 +0330

    locust

--- Locust/locustfile.py
+++ Locust/locustfile.py
@@ -1,4 +1,4 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 class HelloWorldUser(HttpUser):
     def __init__(self, *args, **kwargs):
@@ -7,30 +7,31 @@ class HelloWorldUser(HttpUser):
         self.servernonce = ""
         self.auth_key = ""
 
-    
+    wait_time = between(1, 5)
     @task
     def req_pq(self):
         with self.client.post('/auth/req_pq/'
                          , 
                          json={
-                            'ID': '2',
+                            "ID": 2,
                          }
                     ) as response:
             self.nonce = response.json["nonce"]
             self.servernonce = response.json["serverNonce"]
+          
     
     @task
     def req_dh(self):
-        with self.client.post('/auth/req_DH_params/'
+       with self.client.post('/auth/req_DH_params/'
                          , 
                          json={
-                            'ID': '4',
                             'NONCE': self.nonce,
                             'SERVER_NONCE': self.servernonce,
+                            'ID': 4,
                             'KEY': 6,
                          }
                     ) as response:
-            self.auth_key = response.json["key"]
+            self.auth_key = response.json["key"]  
             
 
     @task
